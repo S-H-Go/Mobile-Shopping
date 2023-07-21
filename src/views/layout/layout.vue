51f0d026946f9e1642b9ec71d1d7ba483510f68f
shg <wangshijiehh@outlook.com>
2023-07-21 09:11:55 +0800

    🐞 fix: 修复刷新页面后，只点击到主页的bug

--- src/views/layout/layout.vue
+++ src/views/layout/layout.vue
@@ -22,14 +22,15 @@
 </template>
 
 <script setup>
-import { onMounted, ref } from "vue";
-import { useRouter } from "vue-router";
+import { onMounted, ref, onUpdated } from "vue";
+import { useRouter, useRoute } from "vue-router";
 import { storeToRefs } from "pinia";
 import { useUserStore } from "@/stores/user";
 const { loginOrNot, user } = storeToRefs(useUserStore());
 const active = ref("home");
 const myStr = ref("");
 const router = useRouter();
+const route = useRoute();
 const toCart = ref("/cart/null");
 onMounted(() => {
   if (loginOrNot.value) {
@@ -39,7 +40,25 @@ onMounted(() => {
     toCart.value = "/cart/null";
     myStr.value = "未登录";
   }
+  //刷新页面后实现点击当前页的效果
+  if (route.path.includes("/home")) {
+    active.value = "home";
+    return;
+  }
+  if (route.path.includes("/category")) {
+    active.value = "category";
+    return;
+  }
+  if (route.path.includes("/cart")) {
+    active.value = "cart";
+    return;
+  }
+  if (route.path.includes("/my")) {
+    active.value = "my";
+    return;
+  }
 });
+
 router.afterEach((to, from) => {
   //从分类页返回的时候，更换为点击首页
   if (from.path.includes("/category") && to.path == "/home") {
